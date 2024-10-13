6eee83f7d79ec1795622bc5de892c41ea4722909
Alessandro Maule <Alessandro824@proton.me>
2024-10-13 16:20:41 +0200

    Upgrade code and dependecies

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/Pwn3rzs/HAK5-C2-License-Toolkit
+
+go 1.23.2
+
+require go.etcd.io/bbolt v1.3.11
+
+require golang.org/x/sys v0.26.0 // indirect
--- main.go
+++ main.go
@@ -8,9 +8,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
-)
 
-import bolt "go.etcd.io/bbolt"
+	bolt "go.etcd.io/bbolt"
+)
 
 type License struct {
 	Key         string `json:"key"`
@@ -39,159 +39,29 @@ type Status struct {
 func main() {
 	fmt.Println("[*] Hak5 C2 Licensing Toolkit by Pwn3rzs / CyberArsenal!")
 	scanner := bufio.NewScanner(os.Stdin)
+
 	db, err := bolt.Open("c2.db", 0600, nil)
 	if err != nil {
+		fmt.Println("[!] Error opening database:", err)
 		return
 	}
-	fmt.Println("[+] DB Opened!")
 	defer db.Close()
+	fmt.Println("[+] DB Opened!")
+
 	for {
 		fmt.Println("[*] Enter a command (generate/decode/read/crack/exit):")
 		scanner.Scan()
 		command := scanner.Text()
+
 		switch command {
 		case "generate":
 			generateHexCode()
 		case "read":
-			err = db.View(func(tx *bolt.Tx) error {
-				fmt.Println("[*] Enter the bucket (setup/status):")
-				scanner.Scan()
-				var bucket string
-				var key string
-				var license License
-				var status Status
-
-				input := scanner.Text()
-				switch input {
-				case "setup":
-					bucket = input
-					key = "license"
-				case "status":
-					bucket = input
-					key = "status"
-				default:
-					return fmt.Errorf("[!] Invalid bucket. Please enter 'setup', 'status'")
-				}
-
-				fmt.Printf("[+] Opening key '%s' in bucket '%s'\n", key, bucket)
-
-				bucketz := tx.Bucket([]byte(bucket))
-				if bucketz == nil {
-					return fmt.Errorf("[!] Bucket '" + bucket + "' not found")
-				}
-
-				data := bucketz.Get([]byte(key))
-				if data == nil {
-					return fmt.Errorf("[!] Data for '" + key + "' not found")
-				}
-
-				decoder := gob.NewDecoder(bytes.NewReader(data))
-				if key == "license" {
-					if err := decoder.Decode(&license); err != nil {
-						return err
-					}
-					prettyLicense, _ := json.MarshalIndent(license, "", "    ")
-
-					fmt.Printf("[+] Decoded License struct: %+v\n", string(prettyLicense))
-				} else {
-					if err := decoder.Decode(&status); err != nil {
-						return err
-					}
-					prettyStatus, _ := json.MarshalIndent(status, "", "    ")
-					fmt.Printf("[+] Decoded Status struct: %+v\n", string(prettyStatus))
-				}
-
-				return nil
-			})
-
-			if err != nil {
-				fmt.Println("[!] Error decoding data:", err)
-			}
+			handleReadCommand(scanner, db)
 		case "decode":
-			fmt.Println("[*] Enter the struct (license/status):")
-			scanner.Scan()
-			mode := scanner.Text()
-			fmt.Println("[*] Enter the hex string:")
-			scanner.Scan()
-			hexData := scanner.Text()
-			decodeHex(hexData, mode)
+			handleDecodeCommand(scanner)
 		case "crack":
-			_ = db.Update(func(tx *bolt.Tx) error {
-				license := License{
-					Key:         "Pwn3rzs",
-					Type:        2,
-					UserLimit:   uint64(10000),
-					DeviceLimit: uint64(10000),
-					SiteLimit:   uint64(10000),
-				}
-
-				var buf bytes.Buffer
-				encoder := gob.NewEncoder(&buf)
-				err := encoder.Encode(license)
-				if err != nil {
-					fmt.Println("[!] Encoding error:", err)
-					return err
-				}
-				//var status Status
-
-				bucketz := tx.Bucket([]byte("setup"))
-				if bucketz == nil {
-					return fmt.Errorf("[!] Bucket 'setup' not found")
-				}
-
-				data := bucketz.Put([]byte("license"), buf.Bytes())
-				if data != nil {
-					return fmt.Errorf("[!] Data for 'license' not found")
-				}
-				var statusView Status
-				bucketz = tx.Bucket([]byte("status"))
-				v := bucketz.Get([]byte("status"))
-				decoder := gob.NewDecoder(bytes.NewReader(v))
-				if err := decoder.Decode(&statusView); err != nil {
-					return err
-				}
-				prettyLicense, _ := json.MarshalIndent(statusView, "", "    ")
-				fmt.Printf("[+] Decoded Status struct: %+v\n", string(prettyLicense))
-				statusView.DeviceLimit = uint64(10000)
-				statusView.UserLimit = uint64(10000)
-				statusView.Edition = "teams"
-				statusView.SiteLimit = uint64(10000)
-				encoder = gob.NewEncoder(&buf)
-				_ = encoder.Encode(statusView)
-				_ = bucketz.Put([]byte("status"), buf.Bytes())
-
-				return nil
-			})
-
-			_ = db.View(func(tx *bolt.Tx) error {
-				var licenseView License
-				b := tx.Bucket([]byte("setup"))
-				v := b.Get([]byte("license"))
-				decoder := gob.NewDecoder(bytes.NewReader(v))
-				if err := decoder.Decode(&licenseView); err != nil {
-					return err
-				}
-				prettyLicense, _ := json.MarshalIndent(licenseView, "", "    ")
-				fmt.Printf("[+] Decoded License struct: %+v\n", string(prettyLicense))
-				return nil
-			})
-
-			_ = db.View(func(tx *bolt.Tx) error {
-				var statusView Status
-				b := tx.Bucket([]byte("status"))
-				v := b.Get([]byte("status"))
-				decoder := gob.NewDecoder(bytes.NewReader(v))
-				if err := decoder.Decode(&statusView); err != nil {
-					return err
-				}
-				prettyStatus, _ := json.MarshalIndent(statusView, "", "    ")
-				fmt.Printf("[+] Decoded Status struct: %+v\n", string(prettyStatus))
-				return nil
-			})
-
-			fmt.Println("[+] DB Values edited")
-			fmt.Println("[*] Patching application")
-
+			handleCrackCommand(db)
 		case "exit":
 			fmt.Println("[!] Exiting...")
 			return
@@ -199,6 +69,140 @@ func main() {
 			fmt.Println("[!] Invalid command. Please enter 'generate', 'decode', 'read', or 'exit'.")
 		}
 	}
+}
+
+func handleReadCommand(scanner *bufio.Scanner, db *bolt.DB) {
+	err := db.View(func(tx *bolt.Tx) error {
+		fmt.Println("[*] Enter the bucket (setup/status):")
+		scanner.Scan()
+		bucket := scanner.Text()
+		key := determineKey(bucket)
+
+		if key == "" {
+			return fmt.Errorf("[!] Invalid bucket. Please enter 'setup' or 'status'")
+		}
+
+		return readData(tx, bucket, key)
+	})
+
+	if err != nil {
+		fmt.Println("[!] Error reading data:", err)
+	}
+}
+
+func determineKey(bucket string) string {
+	switch bucket {
+	case "setup":
+		return "license"
+	case "status":
+		return "status"
+	default:
+		return ""
+	}
+}
+
+func readData(tx *bolt.Tx, bucket string, key string) error {
+	bucketz := tx.Bucket([]byte(bucket))
+	if bucketz == nil {
+		return fmt.Errorf("[!] Bucket '%s' not found", bucket)
+	}
+
+	data := bucketz.Get([]byte(key))
+	if data == nil {
+		return fmt.Errorf("[!] Data for '%s' not found", key)
+	}
+
+	return decodeData(data, key)
+}
+
+func decodeData(data []byte, key string) error {
+	var err error
+	var license License
+	var status Status
+
+	decoder := gob.NewDecoder(bytes.NewReader(data))
+
+	if key == "license" {
+		err = decoder.Decode(&license)
+		if err != nil {
+			return err
+		}
+		prettyLicense, _ := json.MarshalIndent(license, "", "    ")
+		fmt.Printf("[+] Decoded License struct: %+v\n", string(prettyLicense))
+	} else {
+		err = decoder.Decode(&status)
+		if err != nil {
+			return err
+		}
+		prettyStatus, _ := json.MarshalIndent(status, "", "    ")
+		fmt.Printf("[+] Decoded Status struct: %+v\n", string(prettyStatus))
+	}
+
+	return nil
+}
+
+func handleDecodeCommand(scanner *bufio.Scanner) {
+	fmt.Println("[*] Enter the struct (license/status):")
+	scanner.Scan()
+	mode := scanner.Text()
+
+	fmt.Println("[*] Enter the hex string:")
+	scanner.Scan()
+	hexData := scanner.Text()
+
+	if err := decodeHex(hexData, mode); err != nil {
+		fmt.Println("Error decoding:", err)
+	}
+}
+
+func handleCrackCommand(db *bolt.DB) {
+	err := db.Update(func(tx *bolt.Tx) error {
+		license := License{
+			Key:         "Pwn3rzs",
+			Type:        2,
+			UserLimit:   10000,
+			DeviceLimit: 10000,
+			SiteLimit:   10000,
+		}
+
+		if err := saveData(tx, "setup", "license", license); err != nil {
+			return err
+		}
+
+		status := Status{
+			UserLimit:   10000,
+			DeviceLimit: 10000,
+			Edition:     "teams",
+			SiteLimit:   10000,
+		}
+		return saveData(tx, "status", "status", status)
+	})
+
+	if err != nil {
+		fmt.Println("[!] Error updating database:", err)
+	} else {
+		fmt.Println("[+] DB Values edited")
+		fmt.Println("[*] Patching application")
+	}
+}
+
+func saveData(tx *bolt.Tx, bucketName, key string, data interface{}) error {
+	bucketz := tx.Bucket([]byte(bucketName))
+	if bucketz == nil {
+		return fmt.Errorf("[!] Bucket '%s' not found", bucketName)
+	}
+
+	var buf bytes.Buffer
+	encoder := gob.NewEncoder(&buf)
+	if err := encoder.Encode(data); err != nil {
+		return fmt.Errorf("[!] Encoding error: %v", err)
+	}
+
+	if err := bucketz.Put([]byte(key), buf.Bytes()); err != nil {
+		return fmt.Errorf("[!] Error saving data: %v", err)
+	}
+
+	return nil
 }
 
 func generateHexCode() {
@@ -210,47 +214,46 @@ func generateHexCode() {
 		SiteLimit:   500,
 	}
 
-	var buf bytes.Buffer
-	encoder := gob.NewEncoder(&buf)
-	err := encoder.Encode(license)
-	if err != nil {
+	if hexCode, err := encodeToHex(license); err == nil {
+		fmt.Println("[+] Generated Hex Code:", hexCode)
+	} else {
 		fmt.Println("[!] Encoding error:", err)
-		return
 	}
-	fmt.Println("[+] Generated Hex Code:", hex.EncodeToString(buf.Bytes()))
 }
 
-func decodeHex(hexData string, mode string) {
-	var license License
-	var status Status
+func encodeToHex(data interface{}) (string, error) {
+	var buf bytes.Buffer
+	encoder := gob.NewEncoder(&buf)
+	if err := encoder.Encode(data); err != nil {
+		return "", err
+	}
+	return hex.EncodeToString(buf.Bytes()), nil
+}
 
-	switch mode {
-	case "license":
-		hexDecoded, err := hex.DecodeString(hexData)
-		if err != nil {
-			fmt.Println("Error decoding hex string:", err)
-			return
-		}
-		dec := gob.NewDecoder(bytes.NewReader(hexDecoded))
+func decodeHex(hexData string, mode string) error {
+	hexDecoded, err := hex.DecodeString(hexData)
+	if err != nil {
+		return fmt.Errorf("Error decoding hex string: %v", err)
+	}
 
-		if err := dec.Decode(&license); err != nil {
-			fmt.Println("Error decoding gob data:", err)
-			return
+	var decoder *gob.Decoder
+	if mode == "license" {
+		var license License
+		decoder = gob.NewDecoder(bytes.NewReader(hexDecoded))
+		if err := decoder.Decode(&license); err != nil {
+			return fmt.Errorf("Error decoding gob data: %v", err)
 		}
 		fmt.Printf("Decoded License struct: %+v\n", license)
-	case "status":
-		hexDecoded, err := hex.DecodeString(hexData)
-		if err != nil {
-			fmt.Println("Error decoding hex string:", err)
-			return
-		}
-		dec := gob.NewDecoder(bytes.NewReader(hexDecoded))
-		if err := dec.Decode(&status); err != nil {
-			fmt.Println("Error decoding gob data:", err)
-			return
+	} else if mode == "status" {
+		var status Status
+		decoder = gob.NewDecoder(bytes.NewReader(hexDecoded))
+		if err := decoder.Decode(&status); err != nil {
+			return fmt.Errorf("Error decoding gob data: %v", err)
 		}
 		fmt.Printf("Decoded Status struct: %+v\n", status)
-	default:
-		fmt.Println("Invalid command. Please enter 'create', 'decode', or 'exit'.")
+	} else {
+		return fmt.Errorf("Invalid mode: %s. Please enter 'license' or 'status'.", mode)
 	}
+
+	return nil
 }
